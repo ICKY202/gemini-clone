f6f78112b1e3e0cead1bea0e83f31b9019c21240
ICKY202 <vigneshm674@gmail.com>
2025-06-28 19:22:31 +0530

    deployed in docker container

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,10 +3,9 @@
 
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: localStorage.getItem("apiKey")});
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY});
 
 async function main(input) {
-    console.log(input);
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: `${input}`,
